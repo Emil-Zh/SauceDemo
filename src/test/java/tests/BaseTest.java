6499a114fa5ec7f1fdeba6b6eeee9a3ed86f87e5
Emil Zhdanov <zhdanov.emil@gmail.com>
2024-11-28 10:32:47 +0700

    add retry/testng annotations/testing in 3 browsers

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,17 @@
 package tests;
 
+import com.beust.jcommander.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
 import pages.CartPage;
 import pages.CheckOutPage;
 import pages.LoginPage;
@@ -20,13 +27,21 @@ public class BaseTest {
     CartPage cartPage;
     CheckOutPage checkOutPage;
 
+    @Parameters({"browser"})
     @BeforeMethod
-    public void setup(){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    public void setup(String browser){
+        if(browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized");
+            driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        }
+        else if (browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        }
 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
